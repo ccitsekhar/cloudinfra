3a8c80c026a099d990221bca5b897121cf5bd88b
ccitsekhar <rajasekhar.peri@gmail.com>
2025-02-25 20:11:54 +0530

    Update cloudinfra.tf

--- cloudinfra.tf
+++ cloudinfra.tf
@@ -2,11 +2,6 @@ provider "aws" {
   region = "ap-south-2"  # Updated AWS region
 }
 
-# Create an S3 bucket
-resource "aws_s3_bucket" "ccit_s3" {
-  bucket = "ccit-public-bucket"
-  tags = { Name = "ccit-public-bucket321" }
-}
 resource "aws_s3_bucket" "ccit_s321" {
   bucket = "ccit-public-bucket1"
   tags = { Name = "ccit-public-bucket123" }
