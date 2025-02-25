4a6d6fc456a438fa140438a4748d969f75d376f3
ccitsekhar <rajasekhar.peri@gmail.com>
2025-02-25 20:12:55 +0530

    Update cloudinfra.tf

--- cloudinfra.tf
+++ cloudinfra.tf
@@ -6,7 +6,3 @@ resource "aws_s3_bucket" "ccit_s321" {
   bucket = "ccit-public-bucket1"
   tags = { Name = "ccit-public-bucket123" }
 }
-output "s3_bucket_name" {
-description = "The name of the created S3 bucket"
-value = aws_s3_bucket.ccit_s3.id
-}
